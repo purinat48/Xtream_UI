e9c1dad8b5d0cdc2f46d401b2385095921a542f4
purinat48 <71779893+purinat48@users.noreply.github.com>
2020-11-30 08:13:25 +0700

    Update purinat.php

--- purinat.php
+++ purinat.php
@@ -1,3 +1,19 @@
 #EXTM3U
-#EXTINF:-1 tvg-logo="https://www.we-play.live/wp-content/uploads/2019/10/amarintv.jpg" group-title="freetv",Amarin TV
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/fox-action/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxMjo1MTowMiBBTSZoYXNoX3ZhbHVlPVR1WHhFOVZVbkZFMWtNemc1T3B4SEE9PSZ2YWxpZG1pbnV0ZXM9NjA=
+#EXTINF:-1,True Premier Football HD 1
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/tpf1_480/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxMjo1ODo1MyBBTSZoYXNoX3ZhbHVlPVN5aXhPWmZ5eFl1azEvK0ozc2R6QlE9PSZ2YWxpZG1pbnV0ZXM9NjA==
+#EXTINF:-1,FOX Sports HD
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox1/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjAxOjMxIEFNJmhhc2hfdmFsdWU9OXZ2dEg0MFRpMXBwek9KM3lZNko0Zz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,FOX Sports 2
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox2/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjAzOjEzIEFNJmhhc2hfdmFsdWU9Q0J5aVJtdk9yeEhLbmt1YkgzYUFJdz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,FOX Sports 3
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox3/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA0OjE5IEFNJmhhc2hfdmFsdWU9anc4WkM1a01hb0JOcUhxVE5VR1puUT09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,beIN SPORTS 1
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/bein1/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA1OjE2IEFNJmhhc2hfdmFsdWU9VEdXNzZUKytUR3AwN3VBZXNMRHQyUT09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,beIN SPORTS 2
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/bein2/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA2OjE5IEFNJmhhc2hfdmFsdWU9MXVKM2RjZ2h4ZDQ4aFRJNXBuQjJydz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,True Tennis
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/tennis/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA3OjQ5IEFNJmhhc2hfdmFsdWU9UFlEQlByMGJGZVhXSHVHd3pLdzl4Zz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,NBA TV
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/nba/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA5OjE1IEFNJmhhc2hfdmFsdWU9ZnlmbWJvZVdMZUU0Uko2ajV1eHdrZz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,Motor Vision
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/motor-vision/l_5701_73698080_2857.ts?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjEwOjE3IEFNJmhhc2hfdmFsdWU9dHovV1hQWnlEOFRWeDF4cFZIRzY0QT09JnZhbGlkbWludXRlcz02MA==
