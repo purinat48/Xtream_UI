022a61c2d9927cac6ff33aec1caffdd73da7738d
purinat48 <71779893+purinat48@users.noreply.github.com>
2020-11-30 09:15:23 +0700

    Update purinat.php

--- purinat.php
+++ purinat.php
@@ -1,6 +1,6 @@
 #EXTM3U
 #EXTINF:-1,True Premier Football HD 1
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/tpf1_480/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxMjo1ODo1MyBBTSZoYXNoX3ZhbHVlPVN5aXhPWmZ5eFl1azEvK0ozc2R6QlE9PSZ2YWxpZG1pbnV0ZXM9NjA==
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/tpf1_480/chunks.m3u8?nimblesessionid=15612876&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjEzOjI2IEFNJmhhc2hfdmFsdWU9cnp5eW5RZ1dJNSswR29TWHY0ckE3QT09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,FOX Sports HD
 https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox1/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjAxOjMxIEFNJmhhc2hfdmFsdWU9OXZ2dEg0MFRpMXBwek9KM3lZNko0Zz09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,FOX Sports 2
