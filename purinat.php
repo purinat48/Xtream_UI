4d6842680fcb4d52701233d219decc39ae12fdcc
purinat48 <71779893+purinat48@users.noreply.github.com>
2020-11-30 09:22:23 +0700

    Update purinat.php

--- purinat.php
+++ purinat.php
@@ -2,21 +2,21 @@
 #EXTINF:-1,True Premier Football HD 1
 https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/tpf1_480/chunks.m3u8?nimblesessionid=15612876&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjEzOjI2IEFNJmhhc2hfdmFsdWU9cnp5eW5RZ1dJNSswR29TWHY0ckE3QT09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,FOX Sports HD
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox1/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjAxOjMxIEFNJmhhc2hfdmFsdWU9OXZ2dEg0MFRpMXBwek9KM3lZNko0Zz09JnZhbGlkbWludXRlcz02MA==
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox1/chunks.m3u8?nimblesessionid=15613175&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjE3OjE4IEFNJmhhc2hfdmFsdWU9Q2l5a1A4Yk9DS3pHVGJra1F0WXE3Zz09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,FOX Sports 2
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox2/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjAzOjEzIEFNJmhhc2hfdmFsdWU9Q0J5aVJtdk9yeEhLbmt1YkgzYUFJdz09JnZhbGlkbWludXRlcz02MA==
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox2/chunks.m3u8?nimblesessionid=15613217&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjE4OjAwIEFNJmhhc2hfdmFsdWU9Z1lqNHhsR0ZaWEkxUXhxUmlNQmUwdz09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,FOX Sports 3
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox3/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA0OjE5IEFNJmhhc2hfdmFsdWU9anc4WkM1a01hb0JOcUhxVE5VR1puUT09JnZhbGlkbWludXRlcz02MA==
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/fox3/chunks.m3u8?nimblesessionid=15613260&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjE4OjM4IEFNJmhhc2hfdmFsdWU9SzFZMzl2YXlzQlRiV1Noa0JsZWtwUT09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,beIN SPORTS 1
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/bein1/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA1OjE2IEFNJmhhc2hfdmFsdWU9VEdXNzZUKytUR3AwN3VBZXNMRHQyUT09JnZhbGlkbWludXRlcz02MA==
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/bein1/chunks.m3u8?nimblesessionid=15613307&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjE5OjE5IEFNJmhhc2hfdmFsdWU9aFBkSnBxVnl3UVluVWEwbFFMTUoyUT09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,beIN SPORTS 2
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/bein2/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA2OjE5IEFNJmhhc2hfdmFsdWU9MXVKM2RjZ2h4ZDQ4aFRJNXBuQjJydz09JnZhbGlkbWludXRlcz02MA==
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/bein2/chunks.m3u8?nimblesessionid=15613339&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjE5OjQ1IEFNJmhhc2hfdmFsdWU9dERXQ0ZmdFRNenVLejI0dnByVHh6QT09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,True Tennis
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/tennis/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA3OjQ5IEFNJmhhc2hfdmFsdWU9UFlEQlByMGJGZVhXSHVHd3pLdzl4Zz09JnZhbGlkbWludXRlcz02MA==
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/tennis/chunks.m3u8?nimblesessionid=15613570&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjIxOjUwIEFNJmhhc2hfdmFsdWU9T3hWZGp5OFdrQ0VUWXBBaG9VUTZMZz09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,NBA TV
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/nba/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjA5OjE1IEFNJmhhc2hfdmFsdWU9ZnlmbWJvZVdMZUU0Uko2ajV1eHdrZz09JnZhbGlkbWludXRlcz02MA==
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/nba/chunks.m3u8?nimblesessionid=15613463&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjIwOjU2IEFNJmhhc2hfdmFsdWU9MEtxQk5tdmdQNVRiVm93Q3ZUdXBNdz09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,Motor Vision
-https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/motor-vision/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjEwOjE3IEFNJmhhc2hfdmFsdWU9dHovV1hQWnlEOFRWeDF4cFZIRzY0QT09JnZhbGlkbWludXRlcz02MA==
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/motor-vision/chunks.m3u8?nimblesessionid=15613526&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAyOjIxOjIyIEFNJmhhc2hfdmFsdWU9cG1yMEpwMGFkWHNtaVNlNjdsdGMzUT09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,True Sports HD
 https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/thd1/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjIyOjI2IEFNJmhhc2hfdmFsdWU9eW45anZvOVNmeGgrTmJPRTF0RysrZz09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,True Sports HD 2
