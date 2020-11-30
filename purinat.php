f8f0bf04b9f4f2e2f6aeea2b5be8ade60bbbcad2
purinat48 <71779893+purinat48@users.noreply.github.com>
2020-11-30 08:44:00 +0700

    Update purinat.php

--- purinat.php
+++ purinat.php
@@ -23,3 +23,25 @@ https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/thd1/chunks.m3u8?wmsAuthSign
 https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/thd2/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjIzOjQ4IEFNJmhhc2hfdmFsdWU9bDJ2cXo2WkxUVWdKcmVSS1pSYUhydz09JnZhbGlkbWludXRlcz02MA==
 #EXTINF:-1,True Sports HD 3
 https://r4-sn-w5nuxa-o53d.googlevideocdn.com/sports/thd3/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjI0OjQ0IEFNJmhhc2hfdmFsdWU9bVhVWXFmNDBoc1pWYjN6d2o4MzV6UT09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,FOX THAI
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/foxth/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjI4OjUwIEFNJmhhc2hfdmFsdWU9ZFpobDdPMG91TDE4KzJ2c25ZT0lrUT09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,FOX Action Movies
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/fox-action/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjMwOjQ5IEFNJmhhc2hfdmFsdWU9NFdTcEVnTkRBMzRHUlZaMGtLQlNGUT09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,FOX Family Movies
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/fox-fam/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjMxOjU2IEFNJmhhc2hfdmFsdWU9bjNnWmxVMDI1eW1wb0NKbUNqc2FQUT09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,FOX Life
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/fox-life/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjMzOjE0IEFNJmhhc2hfdmFsdWU9MjRweGVKK0pPOFNLWjljbzN0QjdrUT09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,AXN HD
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/axn/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjM0OjIyIEFNJmhhc2hfdmFsdWU9UmxhK3RiaGV4bXc1dTZNWENaQUYydz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,True Film HD
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/truefilmhd/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjM1OjI3IEFNJmhhc2hfdmFsdWU9dUp6Y0hKQ0VpT2Rra3ZaSHNDdlh3QT09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,True X-ZYTE
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/xzyte/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjM2OjQyIEFNJmhhc2hfdmFsdWU9cm4xRFB6QXFtN0pWcmpLTnloeXBqdz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,Hits
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/hits/chunks.m3u8?nimblesessionid=15609896&wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjM4OjE1IEFNJmhhc2hfdmFsdWU9U2RBNmlhMXNnbnJhdUZBVVVjZUdkZz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,HBO HD
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/hbo-hd/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjM5OjA2IEFNJmhhc2hfdmFsdWU9WEpxZ1FSZ0UrcmpBRllDd2puckNwdz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,HBO Signature
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/hbo-sig/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjQwOjM3IEFNJmhhc2hfdmFsdWU9UHByK0hud09KTFhRVzl4M0pONkZSdz09JnZhbGlkbWludXRlcz02MA==
+#EXTINF:-1,FOX Movies 
+https://r4-sn-w5nuxa-o53d.googlevideocdn.com/entertainment/fox-movies-en/chunks.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9MTEvMzAvMjAyMCAxOjQxOjQ1IEFNJmhhc2hfdmFsdWU9dUg3eGk3bnpFaWdrYzNwQnhmYUQ3dz09JnZhbGlkbWludXRlcz02MA==
